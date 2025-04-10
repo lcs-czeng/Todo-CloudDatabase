324a71fa9d3248ffc8619e0ade68c2292b3e941e
Courage Zeng <czeng@lcs.on.ca>
2025-04-10 19:42:38 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,15 @@
+//
+//  Supabas.swift
+//  TodoList
+//
+//  Created by 曾梓恒 on 2025-04-10.
+//
+
+import Foundation
+import Supabase
+ 
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://tttuqjdssoppyvvnuceg.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0dHVxamRzc29wcHl2dm51Y2VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2ODc5NDksImV4cCI6MjA1OTI2Mzk0OX0.BQRiZ-duWRPz677NgbPZWc4kfYuMj_XgKIe5dgfNZUs"
+)
+ 
